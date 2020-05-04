3105095b76c0701da5373d96087327e102dbe7a7
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-04 13:47:55 -0600

    get quiz demo fully working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
+import quizQuestions from "./api/quizQuestions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counter: 0,
+    questionId: 1,
+    question: "",
+    answerOptions: [],
+    answer: "",
+    answersCount: {},
+    result: "",
+  };
+
+  componentDidMount() {
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: quizQuestions[0].answers,
+    });
+  }
+
+  setUserAnswer = (answer) => {
+    this.setState((state) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: (state.answersCount[answer] || 0) + 1,
+      },
+      answer: answer,
+    }));
+  };
+
+  setNextQuestion = () => {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: "",
+    });
+  };
+
+  getResults = () => {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter(
+      (key) => answersCount[key] === maxAnswerCount
+    );
+  };
+
+  setResults = (result) => {
+    if (result.length === 1) {
+      this.setState({ result: result[0] });
+    } else {
+      this.setState({ result: "Undetermined" });
+    }
+  };
+
+  handleAnswerSelected = (event) => {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+      setTimeout(() => this.setNextQuestion(), 300);
+    } else {
+      setTimeout(() => this.setResults(this.getResults()), 300);
+    }
+  };
+
+  renderQuiz = () => {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  };
+
+  renderResult = () => {
+    return <Result quizResult={this.state.result} />;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
 }
 
 export default App;
