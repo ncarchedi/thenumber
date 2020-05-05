f773fd0a53729fac01cdad1b63b2d05954d8ace6
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-05 10:38:35 -0600

    add tagline to header :)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="container header">the number</div>;
+  return (
+    <div className="container header">
+      <div className="logo">the number</div>
+      <div className="tagline">Retirement Planning That Doesn't Suck™</div>
+    </div>
+  );
 };
 
 export default Header;
