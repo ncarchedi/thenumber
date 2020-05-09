de3fc52aa84237348222be79b8c535741ce4479c
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-08 20:34:19 -0600

    start working on create plan feature

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,81 +5,114 @@ import { CSSTransitionGroup } from "react-transition-group";
 import ResultsChart from "./ResultsChart";
 import toDollars from "../utils/toDollars";
 import getResultsChartData from "../utils/getResultsChartData";
+import calculateAnnualContribution from "../utils/calculateAnnualContribution";
 import calculateTargetSavings from "../utils/calculateTargetSavings";
 
-const Results = (props) => {
-  const {
-    currentAge,
-    targetAge,
-    monthlyExpenses,
-    currentSavings,
-    strategy,
-  } = props.userData;
+class Results extends React.Component {
+  state = {
+    showCreatePlan: false,
+  };
 
-  const targetSavings = calculateTargetSavings(
-    currentAge,
-    targetAge,
-    monthlyExpenses
-  );
+  handleCreatePlanClick = () => {
+    this.setState({ showCreatePlan: !this.state.showCreatePlan });
+  };
 
-  const additionalSavings = targetSavings - currentSavings;
-  const yearsToRetirement = targetAge - currentAge;
-
-  const chartData = getResultsChartData(
-    currentAge,
-    targetAge,
-    currentSavings,
-    targetSavings
-  );
-
-  const renderTargetAgeResults = () => {
-    return (
-      <div>
-        <div className="targetSavingsText">{toDollars(targetSavings)}</div>
-        <p className="additionalSavingsText">
-          {`This is the amount you need by age ${targetAge}. You have ${toDollars(
-            currentSavings
-          )} today, so you need to accumulate an additional ${toDollars(
-            additionalSavings
-          )} over the next ${yearsToRetirement} years.`}
-        </p>
-        <ResultsChart
-          ageArray={chartData.ageArray}
-          savingsArray={chartData.savingsArray}
-        />
-        <button className="actionButton">📝 Create a Plan </button>
+  renderCreatePlan = () => {
+    return this.state.showCreatePlan ? (
+      <h2>This is a test</h2>
+    ) : (
+      <div style={{ textAlign: "center" }}>
+        <button className="actionButton" onClick={this.handleCreatePlanClick}>
+          📝 Create a Plan
+        </button>
       </div>
     );
   };
 
-  const renderNotSupportedResults = () => {
-    return (
-      <div>
-        <h2>
-          Sorry, we don't support that option yet! <a href=".">Try again?</a>
-        </h2>
-      </div>
-    );
-  };
+  render() {
+    const {
+      strategy,
+      targetAge,
+      currentAge,
+      monthlyExpenses,
+      currentSavings,
+    } = this.props.userData;
 
-  return (
-    <CSSTransitionGroup
-      className="container results"
-      component="div"
-      transitionName="fade"
-      transitionEnterTimeout={800}
-      transitionLeaveTimeout={500}
-      transitionAppear
-      transitionAppearTimeout={500}
-    >
-      <div className="resultsContainer">
-        {strategy === "targetAge"
-          ? renderTargetAgeResults()
-          : renderNotSupportedResults()}
-      </div>
-    </CSSTransitionGroup>
-  );
-};
+    const targetSavings = calculateTargetSavings(
+      currentAge,
+      targetAge,
+      monthlyExpenses
+    );
+
+    const additionalSavings = targetSavings - currentSavings;
+    const yearsToRetirement = targetAge - currentAge;
+
+    // TODO: un-hardcode this
+    const annualReturn = 0.07;
+    const annualContribution = calculateAnnualContribution(
+      currentSavings,
+      targetSavings,
+      annualReturn,
+      yearsToRetirement
+    );
+
+    const chartData = getResultsChartData(
+      currentAge,
+      targetAge,
+      currentSavings,
+      annualContribution,
+      annualReturn
+    );
+
+    const renderTargetAgeResults = () => {
+      return (
+        <div>
+          <div className="targetSavingsText">{toDollars(targetSavings)}</div>
+          <p className="additionalSavingsText">
+            {`This is the amount you need by age ${targetAge}. You have ${toDollars(
+              currentSavings
+            )} today, so you need to accumulate an additional ${toDollars(
+              additionalSavings
+            )} over the next ${yearsToRetirement} years.`}
+          </p>
+          <ResultsChart
+            ageArray={chartData.ageArray}
+            savingsArray={chartData.savingsArray}
+          />
+        </div>
+      );
+    };
+
+    const renderNotSupportedResults = () => {
+      return (
+        <div>
+          <h2>
+            Sorry, we don't support that option yet! <a href=".">Try again?</a>
+          </h2>
+        </div>
+      );
+    };
+
+    return (
+      <CSSTransitionGroup
+        className="container results"
+        component="div"
+        transitionName="fade"
+        transitionEnterTimeout={800}
+        transitionLeaveTimeout={500}
+        transitionAppear
+        transitionAppearTimeout={500}
+      >
+        <div className="resultsContainer">
+          {strategy === "targetAge"
+            ? renderTargetAgeResults()
+            : renderNotSupportedResults()}
+        </div>
+        <div>{this.renderCreatePlan()}</div>
+      </CSSTransitionGroup>
+    );
+  }
+}
 
 Results.propTypes = {
   userData: PropTypes.exact({
