3b4cdfad7fb1c112d47d1ec42eecf48de56c2b7e
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-11 16:00:44 -0600

    add simple material color theme adjustment

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Feedback from "./pages/Feedback";
 
-class App extends React.Component {
-  state = {
-    name: "",
+const App = () => {
+  const [name, setName] = useState(0);
+
+  const handleSetName = (name) => {
+    setName(name);
   };
 
-  handleSetName = (name) => {
-    this.setState({ name });
-  };
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#59cd90",
+      },
+    },
+  });
 
-  render() {
-    return (
+  return (
+    <ThemeProvider theme={theme}>
       <Router>
         <div className="App">
           <Header />
           <Switch>
             <Route exact path="/">
-              <Home onSetName={this.handleSetName} />
+              <Home onSetName={handleSetName} />
             </Route>
             <Route path="/about">
               <About />
             </Route>
             <Route path="/feedback">
-              <Feedback name={this.state.name} />
+              <Feedback name={name} />
             </Route>
           </Switch>
         </div>
       </Router>
-    );
-  }
-}
+    </ThemeProvider>
+  );
+};
 
 export default App;
