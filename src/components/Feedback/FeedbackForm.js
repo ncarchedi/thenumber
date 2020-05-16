ed24b333d9a0be39c5911b34fc1fbda2fc770335
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-16 09:01:15 -0600

    get feedback css working

--- src/components/Feedback/FeedbackForm.js
+++ src/components/Feedback/FeedbackForm.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-
+import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import SendIcon from "@material-ui/icons/Send";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 const encode = (data) => {
   return Object.keys(data)
@@ -12,6 +21,8 @@ const encode = (data) => {
 };
 
 export default function FeedbackForm(props) {
+  const classes = useStyles();
+
   const [name, setName] = useState(props.name);
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -29,39 +40,49 @@ export default function FeedbackForm(props) {
   };
 
   return (
-    <Container maxWidth="sm" className="feedbackForm">
-      {name ? (
-        <h3 className="feedbackFormTitle">
-          {name}, where should we go from here? 🤔
-        </h3>
-      ) : (
-        <h3 className="feedbackFormTitle">Where should we go from here? 🤔</h3>
-      )}
-      <p>
+    <Container maxWidth="sm">
+      <Typography variant="h6" gutterBottom>
+        Where should we go from here? 🤔
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
         <em>The Number</em> is a work in progress—and you can help shape it!
-      </p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Your email (in case we have questions)"
-        />
-        <textarea
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Write your feedback and/or suggestions here..."
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="default"
-          endIcon={<SendIcon />}
-        >
-          Submit Feedback
-        </Button>
+      </Typography>
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              type="email"
+              name="email"
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              helperText="In case we have any questions"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              name="message"
+              label="Feedback"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              helperText="Let us know how we can improve"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="default"
+              endIcon={<SendIcon />}
+            >
+              Submit Feedback
+            </Button>
+          </Grid>
+        </Grid>
       </form>
     </Container>
   );
