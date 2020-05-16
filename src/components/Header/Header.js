b980952154fefdebc8ab137c4e9d817a480d5413
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-16 17:17:56 -0600

    make feedback link an icon instead

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,18 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import SunIcon from "@material-ui/icons/Brightness7";
 import MoonIcon from "@material-ui/icons/Brightness4";
+import FeedbackIcon from "@material-ui/icons/Feedback";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Logo from "./Logo";
 
 const useStyles = makeStyles((theme) => ({
   logoLink: {
-    textDecoration: "none",
     flexGrow: 1,
-  },
-  feedbackLink: {
     textDecoration: "none",
-    marginRight: theme.spacing(1),
-  },
-  feedbackLinkText: {
-    fontFamily: ["Racing Sans One", "cursive"],
-    color: theme.palette.primary.main,
   },
 }));
 
@@ -35,11 +27,6 @@ export default function Header(props) {
         <Link to="/" className={classes.logoLink}>
           <Logo />
         </Link>
-        <Link to="/feedback" className={classes.feedbackLink}>
-          <Typography variant="subtitle1" className={classes.feedbackLinkText}>
-            give us feedback
-          </Typography>
-        </Link>
         <IconButton
           aria-label="dark mode toggle"
           color="primary"
@@ -47,6 +34,11 @@ export default function Header(props) {
         >
           {darkMode ? <SunIcon /> : <MoonIcon />}
         </IconButton>
+        <Link to="/feedback">
+          <IconButton aria-label="dark mode toggle" color="primary">
+            <FeedbackIcon />
+          </IconButton>
+        </Link>
       </Toolbar>
     </AppBar>
   );
