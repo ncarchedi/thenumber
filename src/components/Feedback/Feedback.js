5ba6785645baae7ac68aef15dcc2db05987c157f
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-16 08:31:00 -0600

    function components in feedback

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -3,16 +3,14 @@ import PropTypes from "prop-types";
 import Container from "@material-ui/core/Container";
 import FeedbackForm from "./FeedbackForm";
 
-const Feedback = (props) => {
+export default function Feedback(props) {
   return (
     <Container>
       <FeedbackForm name={props.name} />
     </Container>
   );
-};
+}
 
 Feedback.propTypes = {
   name: PropTypes.string.isRequired,
 };
-
-export default Feedback;
--- src/components/Feedback/FeedbackForm.js
+++ src/components/Feedback/FeedbackForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import Container from "@material-ui/core/Container";
@@ -11,18 +11,16 @@ const encode = (data) => {
     .join("&");
 };
 
-class FeedbackForm extends React.Component {
-  state = {
-    name: this.props.name,
-    email: "",
-    message: "",
-  };
+export default function FeedbackForm(props) {
+  const [name, setName] = useState(props.name);
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "feedback", ...this.state }),
+      body: encode({ "form-name": "feedback", name, email, message }),
     })
       .then(() => alert("Feedback sent!"))
       .catch((error) => alert(error));
@@ -30,54 +28,45 @@ class FeedbackForm extends React.Component {
     e.preventDefault();
   };
 
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-  render() {
-    const { name, email, message } = this.state;
-    return (
-      <Container maxWidth="sm" className="feedbackForm">
-        {name ? (
-          <h3 className="feedbackFormTitle">
-            {name}, where should we go from here? 🤔
-          </h3>
-        ) : (
-          <h3 className="feedbackFormTitle">
-            Where should we go from here? 🤔
-          </h3>
-        )}
-        <p>
-          <em>The Number</em> is a work in progress—and you can help shape it!
-        </p>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-            placeholder="Your email (in case we have questions)"
-          />
-          <textarea
-            name="message"
-            value={message}
-            onChange={this.handleChange}
-            placeholder="Write your feedback and/or suggestions here..."
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="default"
-            endIcon={<SendIcon />}
-          >
-            Submit Feedback
-          </Button>
-        </form>
-      </Container>
-    );
-  }
+  return (
+    <Container maxWidth="sm" className="feedbackForm">
+      {name ? (
+        <h3 className="feedbackFormTitle">
+          {name}, where should we go from here? 🤔
+        </h3>
+      ) : (
+        <h3 className="feedbackFormTitle">Where should we go from here? 🤔</h3>
+      )}
+      <p>
+        <em>The Number</em> is a work in progress—and you can help shape it!
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Your email (in case we have questions)"
+        />
+        <textarea
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Write your feedback and/or suggestions here..."
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="default"
+          endIcon={<SendIcon />}
+        >
+          Submit Feedback
+        </Button>
+      </form>
+    </Container>
+  );
 }
 
 FeedbackForm.propTypes = {
   name: PropTypes.string.isRequired,
 };
-
-export default FeedbackForm;
