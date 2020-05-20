b1dbde19bdf14b9c4a090815bb98950d1ebb9724
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-20 08:18:06 -0600

    fix content copy

--- src/api/content.js
+++ src/api/content.js
@@ -25,7 +25,7 @@ export default [
     type: "Statement",
     content: {
       prompt:
-        "It may be counterintuitive, but the amount of money you need to retire actually *increases* over time. In short, any money you don't have saved today is money that's not invested and earning a return for tomorrow. 📈",
+        "It may be counterintuitive, but the amount of money you need to retire actually <em>increases</em> over time. In short, any money you don't have saved today is money that's not invested and earning a return for tomorrow. 📈",
     },
   },
   {
@@ -75,4 +75,27 @@ export default [
         "Include cash, investments, retirement accounts, and any other savings that could be used to pay your monthly expenses someday.",
     },
   },
+  // {
+  //   type: "MultipleChoiceQuestion",
+  //   inputType: "string",
+  //   variableName: "annualReturn",
+  //   content: {
+  //     prompt:
+  //       "Which of the following best describes how your savings are allocated? 📈",
+  //     answers: [
+  //       {
+  //         value: "7",
+  //         label: "Mostly stocks",
+  //       },
+  //       {
+  //         value: "5",
+  //         label: "About half stocks",
+  //       },
+  //       {
+  //         value: "3",
+  //         label: "Mostly not stocks",
+  //       },
+  //     ],
+  //   },
+  // },
 ];
