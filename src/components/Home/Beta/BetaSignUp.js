a970bdf60b41b8a0cd06eda4a8b057af3eba02fd
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-21 15:52:15 -0600

    add thank you screen to beta sign up

--- src/components/Home/Beta/BetaSignUp.js
+++ src/components/Home/Beta/BetaSignUp.js
@@ -16,6 +16,12 @@ const useStyles = makeStyles((theme) => ({
   form: {
     marginTop: theme.spacing(3),
   },
+  thanksContainer: {
+    textAlign: "center",
+  },
+  gif: {
+    margin: theme.spacing(4, "auto"),
+  },
 }));
 
 export default function BetaSignUp(props) {
@@ -23,6 +29,7 @@ export default function BetaSignUp(props) {
   const { name } = props;
   const [email, setEmail] = useState("");
   const [feedback, setFeedback] = useState("");
+  const [showThanks, setShowThanks] = useState(false);
 
   const handleSubmit = (e) => {
     fetch("/", {
@@ -31,6 +38,7 @@ export default function BetaSignUp(props) {
       body: encode({ "form-name": "betaSignUp", name, email, feedback }),
     })
       .then(() => console.log("Beta sign up form sent!"))
+      .then(() => setShowThanks(true))
       .catch((error) => alert(error));
 
     e.preventDefault();
@@ -38,51 +46,67 @@ export default function BetaSignUp(props) {
 
   return (
     <Container maxWidth="sm">
-      <Typography variant="h3" component="h1" paragraph>
-        Sign up for our beta <Emoji symbol="🤫" label="shushing face" />
-      </Typography>
-      <Typography variant="subtitle1" paragraph>
-        Thanks for letting us know what you'd like to do next! We're still
-        collecting feedback to figure out what our users want most.
-      </Typography>
-      <Typography variant="subtitle1">
-        <em className={classes.betaText}>
-          In the meantime, leave your email to be notified when we launch our
-          beta...
-        </em>
-      </Typography>
-      <form className={classes.form} onSubmit={handleSubmit}>
-        <TextField
-          type="email"
-          name="email"
-          label="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          variant="outlined"
-          fullWidth
-          required
-        />
-        <TextField
-          name="feedback"
-          label="Feedback (optional)"
-          placeholder="I would love The Number even more if..."
-          value={feedback}
-          onChange={(e) => setFeedback(e.target.value)}
-          variant="outlined"
-          multiline
-          rows={3}
-          margin="normal"
-          fullWidth
-        />
-        <BigButton
-          type="submit"
-          variant="contained"
-          color="primary"
-          endIcon={<NotificationsIcon />}
-        >
-          Get notified
-        </BigButton>
-      </form>
+      {showThanks ? (
+        <div className={classes.thanksContainer}>
+          <Typography variant="h3" component="h1">
+            Thanks, we'll be in touch!
+          </Typography>
+          <img
+            src="https://media.giphy.com/media/WmkqburJqXziM/giphy.gif"
+            alt="carlton dance"
+            className={classes.gif}
+            width="100%"
+          />
+        </div>
+      ) : (
+        <div>
+          <Typography variant="h3" component="h1" paragraph>
+            Sign up for our beta <Emoji symbol="🤫" label="shushing face" />
+          </Typography>
+          <Typography variant="subtitle1" paragraph>
+            Thanks for letting us know what you'd like to do next! We're still
+            collecting feedback to figure out what our users want most.
+          </Typography>
+          <Typography variant="subtitle1">
+            <em className={classes.betaText}>
+              In the meantime, leave your email to be notified when we launch
+              our beta...
+            </em>
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              type="email"
+              name="email"
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              variant="outlined"
+              fullWidth
+              required
+            />
+            <TextField
+              name="feedback"
+              label="Feedback (optional)"
+              placeholder="I would love The Number even more if..."
+              value={feedback}
+              onChange={(e) => setFeedback(e.target.value)}
+              variant="outlined"
+              multiline
+              rows={3}
+              margin="normal"
+              fullWidth
+            />
+            <BigButton
+              type="submit"
+              variant="contained"
+              color="primary"
+              endIcon={<NotificationsIcon />}
+            >
+              Get notified
+            </BigButton>
+          </form>
+        </div>
+      )}
     </Container>
   );
 }
