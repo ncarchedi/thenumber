44c3c3a81d807ab87208f2bd34c8e0c76478ff6a
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-24 14:43:53 -0600

    add tooltips to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 import SunIcon from "@material-ui/icons/Brightness7";
 import MoonIcon from "@material-ui/icons/Brightness4";
 import FeedbackIcon from "@material-ui/icons/Feedback";
@@ -27,20 +28,24 @@ export default function Header(props) {
         <Link to="/" className={classes.logo}>
           <Logo />
         </Link>
-        <IconButton
-          aria-label="dark mode toggle"
-          color="primary"
-          onClick={() => setDarkMode(!darkMode)}
-        >
-          {darkMode ? <SunIcon /> : <MoonIcon />}
-        </IconButton>
-        <IconButton
-          aria-label="give us feedback"
-          color="primary"
-          onClick={() => setOpenFeedbackModal(true)}
-        >
-          <FeedbackIcon />
-        </IconButton>
+        <Tooltip title={darkMode ? "Light Mode" : "Dark Mode"}>
+          <IconButton
+            aria-label="dark mode toggle"
+            color="primary"
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            {darkMode ? <SunIcon /> : <MoonIcon />}
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Give Us Feedback">
+          <IconButton
+            aria-label="give us feedback"
+            color="primary"
+            onClick={() => setOpenFeedbackModal(true)}
+          >
+            <FeedbackIcon />
+          </IconButton>
+        </Tooltip>
       </Toolbar>
     </AppBar>
   );
