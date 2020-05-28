515e43407db2ac1eac38bec48c5fbb8ff760fd53
Nick Carchedi <nick.carchedi@gmail.com>
2020-05-28 07:31:48 -0600

    make landing page more mobile friendly

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
 import BigButton from "../General/BigButton";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
@@ -11,19 +12,26 @@ import Footer from "../Footer/Footer";
 
 const useStyles = makeStyles((theme) => ({
   row: {
-    marginTop: theme.spacing(15),
+    [theme.breakpoints.up("sm")]: {
+      marginTop: theme.spacing(15),
+    },
   },
   header: {
+    fontSize: theme.typography.h3.fontSize,
     fontWeight: theme.typography.fontWeightMedium,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: theme.typography.h2.fontSize,
+    },
   },
   subHeader: {
     color: theme.palette.text.secondary,
     fontWeight: theme.typography.fontWeightRegular,
-    fontSize: "1.8rem",
-    maxWidth: theme.breakpoints.values.md - 250,
+    fontSize: theme.typography.h5.fontSize,
+    maxWidth: theme.breakpoints.values.md - 200,
     marginBottom: theme.spacing(3),
     [theme.breakpoints.up("sm")]: {
       lineHeight: "2.5rem",
+      fontSize: theme.typography.h4.fontSize,
     },
   },
   supportingTextContainer: {
@@ -31,11 +39,30 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
   },
   supportingText: {
+    fontSize: theme.typography.h5.fontSize,
     fontWeight: theme.typography.fontWeightLight,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: theme.typography.h4.fontSize,
+    },
   },
   finalRow: {
     textAlign: "center",
-    marginTop: theme.spacing(10),
+    marginTop: theme.spacing(5),
+    [theme.breakpoints.up("sm")]: {
+      marginTop: theme.spacing(15),
+    },
+  },
+  finalRowText: {
+    fontSize: theme.typography.h5.fontSize,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: theme.typography.h4.fontSize,
+    },
+  },
+  divider: {
+    margin: theme.spacing(5, 10),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
   },
 }));
 
@@ -74,40 +101,43 @@ export default function Home(props) {
           </BigButton>
         </Grid>
       </Grid>
+      <Divider className={classes.divider} />
       <Grid container spacing={6} className={classes.row}>
-        <Grid item xs={4} className={classes.supportingTextContainer}>
+        <Grid item xs={12} md={4} className={classes.supportingTextContainer}>
           <Typography variant="h4" className={classes.supportingText}>
             Answer a series of targeted questions in a fun and conversational
             style, with plenty of help if you need it.
           </Typography>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={12} md={8}>
           <Paper elevation={3} className={classes.imageContainer}>
             <img src="question.png" alt="question example" width="100%" />
           </Paper>
         </Grid>
       </Grid>
+      <Divider className={classes.divider} />
       <Grid container spacing={6} className={classes.row}>
-        <Grid item xs={8}>
+        <Grid item xs={12} md={8}>
           <Paper elevation={3} className={classes.imageContainer}>
             <img src="results-chart.png" alt="chart example" width="100%" />
           </Paper>
         </Grid>
-        <Grid item xs={4} className={classes.supportingTextContainer}>
+        <Grid item xs={12} md={4} className={classes.supportingTextContainer}>
           <Typography variant="h4" className={classes.supportingText}>
             Understand how much you need to save and how long it will take you
             to save it. Plain and simple.
           </Typography>
         </Grid>
       </Grid>
+      <Divider className={classes.divider} />
       <Grid container spacing={6} className={classes.row}>
-        <Grid item xs={4} className={classes.supportingTextContainer}>
+        <Grid item xs={12} md={4} className={classes.supportingTextContainer}>
           <Typography variant="h4" className={classes.supportingText}>
             Tweak your assumptions until you feel confident in your number. Then
             commit to achieving it.
           </Typography>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={12} md={8}>
           <Paper elevation={3} className={classes.imageContainer}>
             <img src="assumptions.png" alt="assumptions example" width="100%" />
           </Paper>
@@ -115,7 +145,11 @@ export default function Home(props) {
       </Grid>
       <Grid container className={classes.finalRow}>
         <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom>
+          <Typography
+            variant="h4"
+            gutterBottom
+            className={classes.finalRowText}
+          >
             Ready to take your first step toward financial independence?
           </Typography>
           <BigButton
